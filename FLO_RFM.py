6b76cc79a3bc6dc12f0b15235e46683e0ca1b47e
Ceyda Gönülalçak <ceydagonulalcakcg@gmail.com>
2023-08-31 21:23:00 +0300

    Update FLO_RFM.py

--- FLO_RFM.py
+++ FLO_RFM.py
@@ -4,35 +4,6 @@
 
 
 ###############################################################
-# Business Problem
-###############################################################
-
-# FLO wants to segment its customers and determine marketing strategies according to these segments.
-# For this, the behavior of the customers will be defined and groups will be formed according to these behavior clusters.
-
-
-###############################################################
-# Dataset Story
-###############################################################
-
-# The dataset is based on the past shopping behavior of customers who made their last purchases as OmniChannel (both online and offline shopper) in 2020 - 2021.
-# consists of the information obtained.
-
-# master_id: Unique customer number
-# order_channel: Which channel was used for shopping (Android, ios, Desktop, Mobile, Offline)
-# last_order_channel: The channel used for the last purchase
-# first_order_date: The date of the customer's first purchase
-# last_order_date: The date of the customer's last purchase
-# last_order_date_online: The date of the customer's last online purchase
-# last_order_date_offline: The date of the customer's last offline purchase
-# order_num_total_ever_online: The total number of purchases made by the customer online
-# order_num_total_ever_offline: The total number of purchases made by the customer offline
-# customer_value_total_ever_offline: The total amount paid by the customer for offline purchases
-# customer_value_total_ever_online: The total amount paid by the customer for online purchases
-# interested_in_categories_12: List of categories the customer shopped in the last 12 months
-
-
-###############################################################
 # TASKS
 ###############################################################
 
@@ -234,3 +205,4 @@ def create_rfm(dataframe):
 
 
 rfm_df = create_rfm(df)
+
